8e166f3aae5092672e11ccabe731e5cd84968574
Alireza <alr.rashidi@outlook.com>
2023-12-18 18:30:49 +0330

    Fix settings checkbox labels bug

--- app/settings/items/CheckboxSetting.tsx
+++ app/settings/items/CheckboxSetting.tsx
@@ -29,7 +29,7 @@ const CheckboxSetting = ({ title, itemId, help, defaultVal }: PropsType) => {
           type="checkbox"
           className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-red-500 checked:bg-red-500 checked:before:bg-red-500 hover:before:opacity-10"
           checked={checked}
-          id="checkbox"
+          id={`checkbox-${itemId}`}
           onChange={() => setChecked((prev) => !prev)}
         />
         <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
@@ -49,7 +49,7 @@ const CheckboxSetting = ({ title, itemId, help, defaultVal }: PropsType) => {
           </svg>
         </span>
       </div>
-      <label className="flex flex-row items-center gap-2" htmlFor="checkbox">
+      <label className="flex flex-row items-center gap-2" htmlFor={`checkbox-${itemId}`}>
         {title}
         {help ? <HelpBubble text={help} /> : null}
       </label>
