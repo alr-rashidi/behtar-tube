12a9dfd6b3d1afa8f3d53c34a0b5a3a8ae093490
Alireza <alr.rashidi@outlook.com>
2023-12-27 14:03:26 +0330

    🌈 style: Mobile click help fix
    Add a delay to the help bubble to prevent it from being displayed with each click on the mobile.

--- components/HelpBubble.tsx
+++ components/HelpBubble.tsx
@@ -5,7 +5,7 @@ type PropsType = {
   text: string;
 }
 
-export const helpBubbleClassName = "absolute w-max z-30 max-w-lg p-2 text-center transition scale-50 -translate-x-1/2 -translate-y-10 bg-black opacity-0 pointer-events-none peer-hover:translate-y-0 bg-opacity-80 peer-hover:scale-100 peer-hover:opacity-100 rounded-xl left-1/2"
+export const helpBubbleClassName = "absolute delay-300 w-max z-30 max-w-lg px-2 py-1 text-center transition scale-50 -translate-x-1/2 -translate-y-10 bg-black opacity-0 pointer-events-none peer-hover:translate-y-0 bg-opacity-80 peer-hover:scale-100 peer-hover:opacity-100 rounded-xl left-1/2"
 
 const HelpBubble = ({text}: PropsType) => {
   return (
