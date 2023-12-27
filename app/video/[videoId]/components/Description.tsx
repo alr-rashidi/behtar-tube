f6ba31c13490c78e7f686c76ef02c7c1431e5d43
Alireza <alr.rashidi@outlook.com>
2023-12-27 20:55:08 +0330

    🦄 refactor: condition for des chars
    add condition to show "More..." button in description when description least have 300 chars

--- app/video/[videoId]/components/Description.tsx
+++ app/video/[videoId]/components/Description.tsx
@@ -3,18 +3,20 @@
 import React, { useState } from "react";
 
 const Description = ({ description }: { description: string }) => {
-  const [showFullText, setShowFullText] = useState<boolean>(false)
+  const [showFullText, setShowFullText] = useState<boolean>(false);
 
   const handleClick = () => {
-    setShowFullText(v => !v)
-  }
+    setShowFullText(v => !v);
+  };
 
   return (
     <div className="flex flex-col gap-2">
       <div className={`${!showFullText && "text-trim text-lines-3"}`}>
-        <div className="default-links" dangerouslySetInnerHTML={{__html: description}}></div>
+        <div className="default-links" dangerouslySetInnerHTML={{ __html: description }}></div>
       </div>
-      <button onClick={handleClick} className="customButton">{!showFullText ? 'More...' : 'Less...'}</button>
+      {description.length <= 300
+        ? <button onClick={handleClick} className="customButton">{!showFullText ? "More..." : "Less..."}</button>
+        : null}
     </div>
   );
 };
