5675cd4d1ea24dddb8b0a6a76c29867d0286237d
Alireza <alr.rashidi@outlook.com>
2023-12-27 20:50:09 +0330

    ✨ feat: Buffered bar added

--- app/video/[videoId]/player/components/TimelineInput.tsx
+++ app/video/[videoId]/player/components/TimelineInput.tsx
@@ -9,6 +9,7 @@ type PropsType = {
 const TimelineInput = ({ videoRef, audioRef, currentTime }: PropsType) => {
   const timelineInputRef = useRef<HTMLInputElement>(null);
   const timelineWatchedRef = useRef<HTMLDivElement>(null);
+  const timelineBufferedRef = useRef<HTMLDivElement>(null);
   const timelineBGRef = useRef<HTMLDivElement>(null);
   const timelineThumbRef = useRef<HTMLDivElement>(null);
 
@@ -31,17 +32,24 @@ const TimelineInput = ({ videoRef, audioRef, currentTime }: PropsType) => {
       timelineThumbRef.current
       && timelineInputRef.current
       && timelineWatchedRef.current
-      && videoRef.current
+      && timelineBufferedRef.current
+      && videoRef.current?.getInternalPlayer()
     ) {
-      let position = `${
+      let playedPosition = `${
         // Timeline width - Thumb width / Video time(max value of input) = One percent of the width of input
         ((timelineInputRef.current.clientWidth - 16)
           / videoRef.current.getDuration())
         // ... * Timeline value (parseInt for TS intellisense)
         * videoRef.current.getCurrentTime()}px`;
-
-      timelineThumbRef.current.style.left = position;
-      timelineWatchedRef.current.style.width = position;
+      let BufferedPosition = `${
+        //  Similar to top code
+        ((timelineInputRef.current.clientWidth - 16)
+          / videoRef.current.getDuration())
+        * videoRef.current.getInternalPlayer().buffered.end(0)}px`;
+      timelineThumbRef.current.style.left = playedPosition;
+      timelineWatchedRef.current.style.width = playedPosition;
+      timelineBufferedRef.current.style.width = BufferedPosition;
+      console.log(BufferedPosition);
     }
   };
 
@@ -57,11 +65,19 @@ const TimelineInput = ({ videoRef, audioRef, currentTime }: PropsType) => {
         className="w-full h-full opacity-0 cursor-pointer peer"
       />
       <div
+        aria-label="timeline background bar"
         ref={timelineBGRef}
         className="absolute left-0 w-full my-auto h-1 bg-gray-500 border-white pointer-events-none top-1/2 -translate-y-1/2"
       >
       </div>
       <div
+        aria-label="timeline buffered bar"
+        ref={timelineBufferedRef}
+        className="absolute left-0 h-1 bg-gray-300 border-white pointer-events-none top-1/2 -translate-y-1/2"
+      >
+      </div>
+      <div
+        aria-label="timeline watched bar"
         ref={timelineWatchedRef}
         className="absolute left-0 h-1 bg-red-500 border-white pointer-events-none top-1/2 -translate-y-1/2"
       >
