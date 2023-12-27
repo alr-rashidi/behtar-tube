a5e13ac7d0ba73e4c08d2045a0cf025c3aad1ab8
Alireza <alr.rashidi@outlook.com>
2023-12-27 20:25:28 +0330

    ✨ feat: Whatched timeline added!

--- app/video/[videoId]/player/components/TimelineInput.tsx
+++ app/video/[videoId]/player/components/TimelineInput.tsx
@@ -8,17 +8,16 @@ type PropsType = {
 };
 const TimelineInput = ({ videoRef, audioRef, currentTime }: PropsType) => {
   const timelineInputRef = useRef<HTMLInputElement>(null);
+  const timelineWatchedRef = useRef<HTMLDivElement>(null);
   const timelineBGRef = useRef<HTMLDivElement>(null);
   const timelineThumbRef = useRef<HTMLDivElement>(null);
 
   const handleTimeline = (value: number) => {
     if (videoRef.current && audioRef.current) {
-      const videoInternalPlayer: Record<string, any> =
-        videoRef.current.getInternalPlayer();
-      const audioInternalPlayer: Record<string, any> =
-        audioRef.current.getInternalPlayer();
-        videoInternalPlayer.currentTime = value;
-        audioInternalPlayer.currentTime = value;
+      const videoInternalPlayer: Record<string, any> = videoRef.current.getInternalPlayer();
+      const audioInternalPlayer: Record<string, any> = audioRef.current.getInternalPlayer();
+      videoInternalPlayer.currentTime = value;
+      audioInternalPlayer.currentTime = value;
     }
     ChangeTimelineThumbPosition();
   };
@@ -29,22 +28,25 @@ const TimelineInput = ({ videoRef, audioRef, currentTime }: PropsType) => {
 
   const ChangeTimelineThumbPosition = () => {
     if (
-      timelineThumbRef.current &&
-      timelineInputRef.current &&
-      videoRef.current
+      timelineThumbRef.current
+      && timelineInputRef.current
+      && timelineWatchedRef.current
+      && videoRef.current
     ) {
-      timelineThumbRef.current.style.left = `${
+      let position = `${
         // Timeline width - Thumb width / Video time(max value of input) = One percent of the width of input
-        ((timelineInputRef.current.clientWidth - 16) /
-          videoRef.current.getDuration()) *
+        ((timelineInputRef.current.clientWidth - 16)
+          / videoRef.current.getDuration())
         // ... * Timeline value (parseInt for TS intellisense)
-        videoRef.current.getCurrentTime()
-      }px`;
+        * videoRef.current.getCurrentTime()}px`;
+
+      timelineThumbRef.current.style.left = position;
+      timelineWatchedRef.current.style.width = position;
     }
   };
 
   return (
-    <div aria-label="Timeline" className="relative w-full h-full">
+    <div aria-label="Timeline" className="relative w-full flex items-center">
       <input
         type="range"
         max={videoRef.current?.getDuration()}
@@ -56,12 +58,14 @@ const TimelineInput = ({ videoRef, audioRef, currentTime }: PropsType) => {
       />
       <div
         ref={timelineBGRef}
-        className="absolute w-full h-1 -translate-y-1/2 bg-gray-500 border-white pointer-events-none top-1/2"
-      ></div>
+        className="absolute left-0 w-full my-auto h-1 bg-gray-500 border-white pointer-events-none top-1/2 -translate-y-1/2"
+      >
+      </div>
       <div
-        ref={timelineBGRef}
-        className="absolute w-full h-1 -translate-y-1/2 bg-gray-500 border-white pointer-events-none top-1/2"
-      ></div>
+        ref={timelineWatchedRef}
+        className="absolute left-0 h-1 bg-red-500 border-white pointer-events-none top-1/2 -translate-y-1/2"
+      >
+      </div>
       <div
         ref={timelineThumbRef}
         className="absolute left-0 w-4 h-4 transition -translate-y-1/2 bg-red-800 border border-red-400 border-opacity-0 rounded-full pointer-events-none peer-active:border-opacity-100 peer-active:scale-125 top-1/2"
