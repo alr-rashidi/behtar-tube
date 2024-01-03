43b6179b8b107fa09a8c96535133568d073844fa
Alireza <alr.rashidi@outlook.com>
2024-01-03 14:41:27 +0330

    🦄 refactor: Improve readability

--- utils/videoTimeFormater.ts
+++ utils/videoTimeFormater.ts
@@ -1,24 +1,21 @@
-
 const videoTimeFormater = (time: number) => {
-  let newTime: string = "00:00"
+  let newTime: string = "00:00";
 
-  time = Math.floor(time)
-  if (time >= 60) {
-    if (time < 60*60) {
-      let min = Math.floor(time/60);
-      let sec = Math.floor(time%60);
-      newTime = `${min >= 10 ? min : '0' + min}:${sec >= 10 ? sec : '0' + sec}`
-    } else {
-      let hour = Math.floor(time/(60*60));
-      let min = Math.floor((time%60*60)/60);
-      let sec = Math.floor((time%60*60)%60);
-      newTime = `${hour}:${min >= 10 ? min : '0' + min}:${sec >= 10 ? sec : '0' + sec}`
-    }
-  } else {
-    newTime = `00:${time >= 10 ? time : '0' + time}`
+  time = Math.floor(time);
+  if (time < 60) { // less than 1 minute
+    newTime = `00:${time >= 10 ? time : "0" + time}`;
+  } else if (time < 60 * 60) { // less than 1 hour
+    let min = Math.floor(time / 60);
+    let sec = Math.floor(time % 60);
+    newTime = `${min >= 10 ? min : "0" + min}:${sec >= 10 ? sec : "0" + sec}`;
+  } else { // more than 1 hour
+    let hour = Math.floor(time / (60 * 60));
+    let min = Math.floor((time % 60 * 60) / 60);
+    let sec = Math.floor((time % 60 * 60) % 60);
+    newTime = `${hour}:${min >= 10 ? min : "0" + min}:${sec >= 10 ? sec : "0" + sec}`;
   }
 
   return newTime;
-}
+};
 
-export default videoTimeFormater
+export default videoTimeFormater;
