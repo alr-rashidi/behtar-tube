5806c49b0434c9d38a93f0fcab746b3ec9194232
Alireza <alr.rashidi@outlook.com>
2024-01-03 14:38:10 +0330

    🦄 refactor: Improve Readability

--- utils/numberCounter.ts
+++ utils/numberCounter.ts
@@ -1,30 +1,16 @@
 const numberCounter = (viewCount: number) => {
   // Views = v
   let v;
-  if (viewCount >= 1000) {
-    if (viewCount >= 1000000) {
-      if (viewCount >= 1000000000) {
-        // Billiard
-        if (viewCount >= 10000000000) {
-          // v >= 10B
-          v = (viewCount / 1000000000).toFixed() + "B";
-        } else {
-          v = (viewCount / 1000000000).toFixed(1) + "B";
-        }
-      } else {
-        // Million
-        if (viewCount >= 10000000) {
-          // v >= 10M
-          v = (viewCount / 1000000).toFixed() + "M";
-        } else {
-          v = (viewCount / 1000000).toFixed(1) + "M";
-        }
-      }
-    } else {
-      v = (viewCount / 1000).toFixed() + "K";
-    }
-  } else {
+  if (viewCount < 1000) { // less than 1k
     v = viewCount.toFixed();
+  } else if (viewCount < 1000000) { // less than 1M
+    v = (viewCount / 1000).toFixed() + "K";
+  } else if (viewCount < 1000000000) { // less than 1B
+    let viewsLessThan10M = viewCount < 10000000 ? 1 : 0;
+    v = (viewCount / 1000000).toFixed(viewsLessThan10M ? 1 : 0) + "M";
+  } else { // More than 1B
+    let viewsLessThan10B = viewCount < 1000000000 ? 1 : 0;
+    v = (viewCount / 1000000000).toFixed(viewsLessThan10B ? 1 : 0) + "B";
   }
 
   return v;
