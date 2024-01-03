d43476da50e7c1424e06c9d2f01a1b2f280fe5ac
Alireza <alr.rashidi@outlook.com>
2024-01-03 14:17:01 +0330

    🦄 refactor: Improve code readability

--- utils/dateCounter.ts
+++ utils/dateCounter.ts
@@ -1,44 +1,34 @@
 const dateCounter = (dateNum: number) => {
   const date = dateNum * 1000;
-  const now = new Date().getTime();
-  const d = (now - date) / 1000;
-  let publishText;
+  const currentTime = new Date().getTime();
+  const diffInSeconds = Math.abs((currentTime - date) / 1000);
 
-  const publishTextFormater = (number: number, timeRange: string) => {
-    publishText = `${number} ${timeRange}${number > 1 ? "s" : ""} ago`;
-  };
-
-  // Published time = p & difference = d
-  let p;
-  if (d >= 60) {
-    if (d >= 60 * 60) {
-      if (d >= 60 * 60 * 24) {
-        if (d >= 60 * 60 * 24 * 30) {
-          if (d >= 60 * 60 * 24 * 30 * 12) {
-            p = Math.floor(d / (60 * 60 * 24 * 30 * 12));
-            publishTextFormater(p, "year");
-          } else {
-            p = Math.floor(d / (60 * 60 * 24 * 30));
-            publishTextFormater(p, "month");
-          }
-        } else {
-          p = Math.floor(d / (60 * 60 * 24));
-          publishTextFormater(p, "day");
-        }
-      } else {
-        p = Math.floor(d / (60 * 60));
-        publishTextFormater(p, "hour");
-      }
-    } else {
-      p = Math.floor(d / 60);
-      publishTextFormater(p, "minute");
-    }
-  } else {
-    p = Math.floor(d);
-    publishTextFormater(p, "second");
+  function formatPublishText(num: number, timeUnit: string) {
+    const isPlural = num > 1;
+    return `${num} ${timeUnit}${isPlural ? "s" : ""} ago`;
   }
 
-  return publishText;
+  let published;
+
+  if (!(diffInSeconds >= 60)) { // less than 1 minute
+    published = Math.floor(diffInSeconds);
+    return formatPublishText(published, "second");
+  } else if (!(diffInSeconds >= 60 * 60)) { // less than 1 hour
+    published = Math.floor(diffInSeconds / 60);
+    return formatPublishText(published, "minute");
+  } else if (!(diffInSeconds >= 60 * 60 * 24)) { // less than 1 day
+    published = Math.floor(diffInSeconds / (60 * 60));
+    return formatPublishText(published, "hour");
+  } else if (!(diffInSeconds >= 60 * 60 * 24 * 30)) { // less than 1 month
+    published = Math.floor(diffInSeconds / (60 * 60 * 24));
+    return formatPublishText(published, "day");
+  } else if (!(diffInSeconds >= 60 * 60 * 24 * 30 * 12)) { // less than 1 year
+    published = Math.floor(diffInSeconds / (60 * 60 * 24 * 30));
+    return formatPublishText(published, "month");
+  } else { // less than 1 year
+    published = Math.floor(diffInSeconds / (60 * 60 * 24 * 30 * 12));
+    return formatPublishText(published, "year");
+  }
 };
 
 export default dateCounter;
