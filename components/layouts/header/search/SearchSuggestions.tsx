c10d24b47344cb5a6127812fc0de002ca84cc549
Alireza <alr.rashidi@outlook.com>
2024-01-11 18:48:21 +0330

    🐞 fix: encode link text "utf-8"

--- components/layouts/header/search/SearchSuggestions.tsx
+++ components/layouts/header/search/SearchSuggestions.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import { searchSuggestionsType } from "@/types";
-import React, { useEffect, useRef, useState } from "react";
 import { getSearchSuggestionsData } from "@/api/getYTData";
+import { searchSuggestionsType } from "@/types";
 import localTextDecoder from "@/utils/textDecoder";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+import React, { useEffect, useRef, useState } from "react";
 
 type PropsType = {
   inputText: string;
 };
 const SearchSuggestions = ({ inputText }: PropsType) => {
-  const router = useRouter();
-
   const getSuggestionsTimeout = useRef<NodeJS.Timeout | null>(null);
 
   const [data, setData] = useState<searchSuggestionsType | null>(null);
@@ -28,7 +26,7 @@ const SearchSuggestions = ({ inputText }: PropsType) => {
         try {
           const newData: searchSuggestionsType = await getSearchSuggestionsData(
             inputText,
-            controller.signal
+            controller.signal,
           );
           setData(newData);
         } catch {
@@ -44,24 +42,23 @@ const SearchSuggestions = ({ inputText }: PropsType) => {
     }, 1000);
   }, [inputText]);
 
-  const handleClick = (item: string) => {
-    router.push("/search?q=" + item);
-  };
-
   return (
-    <ul className='absolute left-0 flex-col hidden w-full gap-2 p-2 text-right transition bg-white rounded-lg shadow peer-focus:flex active:flex dark:bg-black backdrop-blur bg-opacity-60 dark:bg-opacity-60'>
-      {!loading ? (
-        data &&
-        data.suggestions.map((item) => (
-          <li className="text-trim text-lines-1" key={item}>
-            <button onClick={() => handleClick(item)}>
-              {localTextDecoder(item)}
-            </button>
-          </li>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
+    <ul className="absolute left-0 flex-col hidden w-full gap-2 p-2 text-right transition bg-white rounded-lg shadow peer-focus:flex active:flex dark:bg-black backdrop-blur bg-opacity-60 dark:bg-opacity-60">
+      {!loading
+        ? (
+          data?.suggestions
+          && data.suggestions.map((item, index) => (
+            <li
+              className={`text-trim text-lines-1 border-gray-300 dark:border-gray-700 ${index+1 != data.suggestions.length ? "border-b" : ""} py-0.5`}
+              key={item}
+            >
+              <Link href={"/search?q=" + localTextDecoder(item)}>
+                {localTextDecoder(item)}
+              </Link>
+            </li>
+          ))
+        )
+        : <p>Loading...</p>}
     </ul>
   );
 };
