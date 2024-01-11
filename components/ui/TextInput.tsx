10cc59eedd0aba6ac5e4bbd9b885c4e4e1c6b05a
Alireza <alr.rashidi@outlook.com>
2024-01-11 20:00:59 +0330

    🦄 refactor: forward ref for TextInput

--- components/ui/TextInput.tsx
+++ components/ui/TextInput.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 type PropsType = {
   className?: string;
   [key: string]: any;
 };
-const TextInput = ({ className: classNameProp, children, ...attributes }: PropsType) => {
+const TextInput = forwardRef<HTMLInputElement, PropsType>(function input({ className: classNameProp, children, ...attributes }: PropsType, ref) {
   return (
     <input
       type="text"
+      ref={ref}
       className={`${classNameProp} bg-slate-200 p-3 dark:bg-slate-800 text-sm h-full rounded-lg hover:brightness-95 focus:brightness-90 border border-transparent focus:border-gray-500 delay-75`}
       {...attributes}
     />
   );
-};
+});
 
 export default TextInput;
