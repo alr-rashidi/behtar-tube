32ed1f37f62795f896c1ce2c89aa9804f4118869
Alireza <alr.rashidi@outlook.com>
2024-01-24 18:43:57 +0330

    🌈 style: disabled cursor style

--- components/ui/TextInput.tsx
+++ components/ui/TextInput.tsx
@@ -9,7 +9,7 @@ const TextInput = forwardRef<HTMLInputElement, PropsType>(function input({ class
     <input
       type="text"
       ref={ref}
-      className={`${classNameProp} bg-slate-200 p-3 dark:bg-slate-800 text-sm h-full rounded-lg hover:brightness-95 focus:brightness-90 border border-transparent focus:border-gray-500 delay-75`}
+      className={`${classNameProp} bg-slate-200 p-3 dark:bg-slate-800 text-sm h-full rounded-lg hover:brightness-95 focus:brightness-90 border border-transparent focus:border-gray-500 delay-75 disabled:text-gray-400 disabled:cursor-not-allowed`}
       {...attributes}
     />
   );
