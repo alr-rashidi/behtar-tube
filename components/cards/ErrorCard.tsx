ad84eabbb7fbfaa389a57c50ef1666a47c84fbbc
Alireza <alr.rashidi@outlook.com>
2024-01-26 19:34:44 +0330

    🦄 refactor: add body part

--- components/cards/ErrorCard.tsx
+++ components/cards/ErrorCard.tsx
@@ -5,9 +5,11 @@ import React from "react";
 import Button from "../ui/Button";
 
 type PropsType = {
-  errorText?: string;
+  title?: string;
+  body?: string;
+  tryBtn?: boolean;
 };
-const ErrorCard = ({ errorText }: PropsType) => {
+const ErrorCard = ({ title, body, tryBtn }: PropsType) => {
   const router = useRouter();
 
   const handleClick = () => {
@@ -16,8 +18,11 @@ const ErrorCard = ({ errorText }: PropsType) => {
 
   return (
     <div className="flex flex-col gap-2 items-center">
-      <div className="text-3xl font-bold">{errorText || "Error!"}</div>
-      <Button onClick={handleClick}>Try again</Button>
+      <div className="text-3xl font-bold">{title || "Error!"}</div>
+      <div>{body}</div>
+      {tryBtn != false
+        ? <Button onClick={handleClick}>Try again</Button>
+        : null}
     </div>
   );
 };
