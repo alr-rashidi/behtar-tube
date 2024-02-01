0d19468f9cf23b1e34a1a0e2e86e51fcfa6b34e3
Alireza <alr.rashidi@outlook.com>
2024-02-01 15:08:21 +0330

    🦄 refactor: Error handling in parent
    of account-form.ts

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,6 +2,7 @@ import { Database } from "@/types/supabase";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import AccountForm from "./account-form";
+import { ErrorCard } from "@/components/cards";
 
 export default async function Account() {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -10,5 +11,15 @@ export default async function Account() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  return <AccountForm user={user} />;
+  if (user == null) {
+    return (
+      <ErrorCard
+        title="User doesn't exist!"
+        body="Sign in if you are not logged in, if not you can try again"
+        tryBtn={false}
+      />
+    );
+  } else {
+    return <AccountForm user={user} />;
+  }
 }
