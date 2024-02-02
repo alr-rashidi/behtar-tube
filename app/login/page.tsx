f725856910599adddfea57d16490ba1f198473e6
Alireza <alr.rashidi@outlook.com>
2024-02-02 16:35:27 +0330

    ✨ feat: add google provider

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -26,7 +26,7 @@ export default function AuthForm() {
           },
         }}
         theme={theme}
-        providers={["github"]}
+        providers={["github", "google"]}
         redirectTo="http://localhost:3000/auth/callback"
       />
     </div>
