654845e21bee51cc6e3572a3b9d611f63a383277
Alireza <alr.rashidi@outlook.com>
2024-02-02 19:51:51 +0330

    🐞 fix: wrong classNames

--- components/ui/Checkbox.tsx
+++ components/ui/Checkbox.tsx
@@ -8,10 +8,10 @@ const Checkbox = ({ ...attributes }: PropsType) => {
     <div className="relative flex items-center p-3 rounded-full cursor-pointer">
       <input
         type="checkbox"
-        className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-neutral-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-tranneutral-y-2/4 before:-tranneutral-x-2/4 before:rounded-full before:bg-blue-neutral-500 before:opacity-0 before:transition-opacity checked:border-red-500 checked:bg-red-500 checked:before:bg-red-500 hover:before:opacity-10"
+        className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-neutral-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-neutral-500 before:opacity-0 before:transition-opacity checked:border-red-500 checked:bg-red-500 checked:before:bg-red-500 hover:before:opacity-10"
         {...attributes}
       />
-      <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -tranneutral-y-2/4 -tranneutral-x-2/4 peer-checked:opacity-100">
+      <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-3.5 w-3.5"
