4583e121f729b637e2bc5228ed212c1dddd83d90
Alireza <alr.rashidi@outlook.com>
2024-02-02 14:14:42 +0330

    🧹 cleanup: Remove UnUsed watermark

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -18,5 +18,3 @@ const Button = ({ children, className: classNameProp, ...attributes }: PropsType
 };
 
 export default Button;
-
-// <!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com -->
