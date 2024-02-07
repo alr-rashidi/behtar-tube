7b4f2a81dd9751e417da404badd9e2594bf40d9b
Alireza <alr.rashidi@outlook.com>
2024-02-07 18:32:53 +0330

    🐞 fix: Handle errors

--- app/subscribes/page.tsx
+++ app/subscribes/page.tsx
@@ -2,45 +2,61 @@
 import { getChannelVideos } from "@/api/getYTData";
 import { getSubscribesList } from "@/api/supabase";
 import { ErrorCard } from "@/components/cards";
+import { VideoListItem } from "@/components/listItems";
 import { VideoType } from "@/types";
 import { Database } from "@/types/supabase";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import React, { useCallback, useEffect, useState } from "react";
 import Loading from "../Loading";
-import { VideoListItem } from "@/components/listItems";
 
 const Page = () => {
-  const [videos, setVideos] = useState<VideoType[]>();
-  const [loading, setLoading] = useState<boolean>(true);
+  type VideosStateType = {
+    error: boolean;
+    loading: boolean;
+    data?: VideoType[];
+  };
+  const [videos, setVideos] = useState<VideosStateType>({ error: false, loading: true });
   const supabase = createClientComponentClient<Database>();
 
-  const getVideosList = useCallback(async () => {
+  const getVideosList = useCallback(async (signal: AbortSignal) => {
     const {
       data: { user: userAuth },
     } = await supabase.auth.getUser();
-    const receivedSubscribes = await getSubscribesList(userAuth?.id!);
-    receivedSubscribes!.map(async item => {
-      const { videos: channelVideos } = await getChannelVideos(item.subscribed_id);
-      setVideos(value => value ? [...value, ...channelVideos].sort((a, b) => b.published - a.published) : [...channelVideos]);
-      console.log(videos);
+    const receivedSubscribes = await getSubscribesList(userAuth?.id!, signal)
+      .catch(err => setVideos(value => ({ ...value, error: true })));
+    receivedSubscribes?.map(async item => {
+      const data = await getChannelVideos(item.subscribed_id, signal)
+        .catch(err => setVideos(value => ({ ...value, error: true })));
+        if (data?.videos) {
+          setVideos(value => {
+            let newVideosState;
+            if (value.data) {
+              newVideosState = [...value.data, ...data.videos];
+            } else {
+              newVideosState = [...data.videos];
+            }
+            return { ...value, data: newVideosState.sort((a, b) => b.published - a.published) };
+          });
+        }
     });
-    setLoading(false);
+    setVideos(value => ({ ...value, loading: false }));
   }, [supabase.auth]);
 
   useEffect(() => {
-    getVideosList();
+    const controller = new AbortController();
+    getVideosList(controller.signal);
+    return () => {
+      controller.abort();
+    };
   }, [getVideosList]);
 
-  if (loading) {
-    return <Loading text="Loading Subscribes videos..." />;
-  } else if (!videos) {
-    return <ErrorCard title="Failed to load subscribes videos" />;
-  }
+  if (videos.error) return <ErrorCard title="Get videos failed" />;
+  if (videos.loading) return <Loading text="Loading Subscribes videos..." />;
+
   return (
     <div>
-      <h1>Videos list:</h1>
       <ul className="flex flex-col gap-4">
-        {videos.map(item => (
+        {videos.data?.map(item => (
           <li key={item.title}>
             <VideoListItem video={item} />
           </li>
